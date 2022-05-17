fe8a36f9eaa2ab6c22d756135793d531c6793b27
Srivishnu Pyda <Srivishnu_Pyda@Srikars-MacBook-Pro.local>
2022-05-17 00:29:03 -0400

    tested out pitch increase/decrease with and without speedups

--- README.md
+++ README.md
@@ -13,3 +13,8 @@ Workflow
 
 1. Split audio files using online tool that applies spleeter (was having dependencies issues on my laptop)
 
+2. Shift vocal pitch up and down 
+
+pitchshifter -s ./hook_interview/input_files/vocals_[ezstems.com].wav -o ./hook_interview/manipulated_vocal_files/vocals_pitched_up.wav -p 7 -b 1
+pitchshifter -s ./hook_interview/input_files/vocals_[ezstems.com].wav -o ./hook_interview/manipulated_vocal_files/vocals_pitched_down.wav -p -7 -b 1
+
--- manipulate_audio.py
+++ manipulate_audio.py
@@ -3,11 +3,36 @@ from pydub import AudioSegment
 from pydub.playback import play
 import sys
 
-
+testing = True
 
 # Load into PyDub
 vocals_stem = AudioSegment.from_wav("input_files/vocals_[ezstems.com].wav")
 accompaniment_stem = AudioSegment.from_wav("input_files/accompaniment_[ezstems.com].wav")
+
+### Testing Effects ###
+
+sound = vocals_stem
+
+print(sound.frame_rate)
+
+# shift the pitch down by half an octave (speed will decrease proportionally)
+octaves = -0.95
+
+#new_sample_rate = int(sound.frame_rate * (2.0 ** octaves))
+low_sample_rate = 32 * 1000
+high_sample_rate = 48 * 1000
+
+lowpitch_sound = sound._spawn(sound.raw_data, overrides={'frame_rate': low_sample_rate})
+highpitch_sound = sound._spawn(sound.raw_data, overrides={'frame_rate': high_sample_rate})
+
+mixed_vocals = lowpitch_sound[:10000].overlay(sound[:10000])
+
+#Play pitch changed sound
+play(mixed_vocals)
+
+if testing:
+    sys.exit(0)
+
 
 # Mix with our original loop
 mixed = vocals_stem.overlay(accompaniment_stem)
